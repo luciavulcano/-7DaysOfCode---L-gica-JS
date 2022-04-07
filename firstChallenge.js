e87d9caf8cd66e54192e7d2d27debf4fe6d883e7
Lucia Vulcano <60199313+luciavulcano@users.noreply.github.com>
2022-04-07 14:07:51 -0300

    Add files via upload

---
+++ firstChallenge.js
@@ -0,0 +1,25 @@
+let numberOne = 1
+let stringOne = '1'
+let numberThirty = 30
+let stringThirty = '30'
+let numberTen = 10
+let stringTen = '10'
+
+
+if (numberOne == stringOne) {
+  console.log('The variables numberOne and stringOne has the same value, but different type')
+} else {
+  console.log('The variables numberOne and stringOne do not have the same value')
+}
+
+if (numberThirty === stringThirty) {
+  console.log('The variables numberThirty and stringThirty has the same value and type')
+} else {
+  console.log('The variables numberThirty and stringThirty do not have the same type')
+}
+
+if (numberTen == stringTen) {
+  console.log('The variables numberTen and stringTen has the same value, but different type')
+} else {
+  console.log('The variables numberTen and stringTen do not have the same value')
+}
